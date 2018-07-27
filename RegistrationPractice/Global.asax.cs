b8fa225cb3c7d160c99ddf2623074dd6af83b0c6
Allan Rodkin <allanrodkin@hotmail.com>
2018-07-27 15:04:11 -0500

    simplinjector

--- RegistrationPractice/Global.asax.cs
+++ RegistrationPractice/Global.asax.cs
@@ -1,12 +1,15 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Reflection;
 using System.Text.RegularExpressions;
 using System.Web;
 using System.Web.Mvc;
 using System.Web.Optimization;
 using System.Web.Routing;
-
+using SimpleInjector;
+using SimpleInjector.Integration.Web;
+using SimpleInjector.Integration.Web.Mvc;
 
 namespace RegistrationPractice
 {
@@ -43,7 +46,19 @@ namespace RegistrationPractice
             FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
             RouteConfig.RegisterRoutes(RouteTable.Routes);
             BundleConfig.RegisterBundles(BundleTable.Bundles);
-            
+
+            var container = new SimpleInjector.Container();
+            container.Options.DefaultScopedLifestyle = new WebRequestLifestyle();
+            // Register your stuff here  
+            container.Register(typeof(IRepository<>), typeof(EFRepository<>).Assembly, Lifestyle.Scoped);
+
+            container.Register(typeof(ApplicationUserManager));
+            container.Register(typeof(ApplicationSignInManager));
+
+            container.RegisterMvcControllers(Assembly.GetExecutingAssembly());
+            //container.RegisterMvcIntegratedFilterProvider();
+            container.Verify();
+            DependencyResolver.SetResolver(new SimpleInjectorDependencyResolver(container));
         }
     }
 }
